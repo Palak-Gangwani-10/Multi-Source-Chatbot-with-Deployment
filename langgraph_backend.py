a260b24bf38b98f1822028ccfc73c6da035fd066
Palak-Gangwani-10 <palak.gangwani.98@gmail.com>
2025-08-14 23:42:56 +0530

    Backend ready

---
+++ langgraph_backend.py
@@ -0,0 +1,46 @@
+# import libraries
+from langgraph.graph import StateGraph, START, END
+from typing import TypedDict, Annotated
+from langchain_core.messages import BaseMessage, HumanMessage
+from langchain_groq import ChatGroq
+import os
+from langgraph.checkpoint.memory import InMemorySaver
+from langgraph.graph.message import add_messages
+from dotenv import load_dotenv
+
+
+class ChatState(TypedDict):
+
+    messages: Annotated[list[BaseMessage], add_messages]
+
+groq_key = os.environ["GROQ_API_KEY"]
+
+# Create model
+model = ChatGroq(
+    api_key=groq_key,
+    model="llama3-70b-8192"
+)
+
+# Checkpointer
+checkpointer = InMemorySaver()
+
+def chat_node(state: ChatState):
+
+    # take user query from state
+    messages = state['messages']
+
+    # send to llm
+    response = model.invoke(messages)
+
+    # response store state
+    return {'messages': [response]}
+
+graph = StateGraph(ChatState)
+
+# add nodes
+graph.add_node('chat_node', chat_node)
+
+graph.add_edge(START, 'chat_node')
+graph.add_edge('chat_node', END)
+
+chatbot = graph.compile(checkpointer=checkpointer)
