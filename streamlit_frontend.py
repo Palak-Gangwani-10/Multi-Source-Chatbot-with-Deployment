1591e868dbecbb819a65df8c0f2ff8acbd698e1e
Palak-Gangwani-10 <palak.gangwani.98@gmail.com>
2025-08-18 23:25:56 +0530

    Basic frontend

--- streamlit_frontend.py
+++ streamlit_frontend.py
@@ -1,7 +1,21 @@
 import streamlit as st
 
-with st.chat_message('user'):
-    st.text('Hi')
+message_history = []
 
-with st.chat_message('assistant'):
-    st.text('How may I help you?')
+#loading the conversation history 
+for message in message_history:
+    with st.chat_message(message['role']):
+        st.text(message['content'])
+
+user_input = st.chat_input('Type here')
+
+if user_input:
+
+    # first add the message to message_history
+    message_history.append({'role':'user', 'content': user_input})
+    with st.chat_message('user'):
+        st.text(user_input)
+
+    message_history.append({'role':'assistant', 'content': user_input})
+    with st.chat_message('assistant'):
+        st.text(user_input)
